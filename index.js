2a122fe4aa5b93941bd5ed767d115da23cea67d5
omarhiguera <108424165+omarhiguera@users.noreply.github.com>
2022-11-23 16:34:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let btn_menu = document.querySelector(".btn-menu")
+let menu_principal = document.querySelector(".nav-tlf-principal")
+let equis =  document.querySelector("#equis")
+
+
+btn_menu.addEventListener("click", () =>{
+    menu_principal.style.display = "block"
+    btn_menu.style.display = "none"
+})
+
+equis.addEventListener("click", () => {
+    menu_principal.style.display = "none"
+    btn_menu.style.display = "block"
+})
+
+
+ /*   if (menu_principal.style.display = "block") {
+    btn_menu.style.display = "none"
+} else {
+    btn_menu.style.display = "block"
+} */
